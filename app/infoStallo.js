78d76e207fd29f2bc6c9a082399b3768f691d7f4
siegluke <luca.claus02@gmail.com>
2024-06-10 09:19:14 +0200

    small fix

--- app/infoStallo.js
+++ app/infoStallo.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 router.post('', async (req, res) => {
     const id = req.body.id;
-    let numPostiLiberi = Math.floor(Math.random()*11);
-    let numBiciDisponibili = Math.floor(Math.random()*11);
+    let numPostiLiberi = await Math.floor(Math.random()*11);
+    let numBiciDisponibili = await Math.floor(Math.random()*11);
     
     res.status(200).json({ message: 'Id received successfully', body: {numPostiLiberi, numBiciDisponibili}});
 });
